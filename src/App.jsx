e3a00776786982ae3a519aacfb91d0026ad2a6fc
LeonAli5366 <leonali274784@gmail.com>
2023-07-28 02:32:14 +0600

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="app">
+      <Header></Header>
+      <Hero></Hero>
+    </div>
   );
 }
 
